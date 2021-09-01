a0194553946f906059cf2923a279ecf5241e5e6a
Kenny Nguyen <kenny@prontotv.no>
2021-09-01 11:05:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "https://staging.adserver.csdm.cloud/external";
+const IFRAME_ID = "iframe-webcontainer";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    window.addEventListener("message", this.receiveMessage);
+    console.log("Hello");
+  }
+  componentWillUnmount() {
+    window.removeEventListener("message", this.receiveMessage);
+  }
+  receiveMessage = (event) => {
+    const {
+      data: { eventType, iframeId },
+    } = event;
+    // Check if the message is for this particular iframe!
+    if (iframeId !== IFRAME_ID) {
+      return;
+    }
+    switch (eventType) {
+      case "READY": {
+        // Add is ready, start ad!
+        const msg = {
+          eventType: "START",
+          iframeId: IFRAME_ID,
+        };
+        this.sendMessage(msg);
+        break;
+      }
+      case "PLAYING": {
+        // Ad is playing.
+        console.log("playing");
+        break;
+      }
+      case "COMPLETE": {
+        console.log("finished");
+        // Ad is finished. Proceed to next ad.
+        break;
+      }
+      default:
+    }
+  };
+
+  onLoad = () => {
+    const configuration = {
+      eventType: "CONFIGURATION_UPDATE",
+      iframeId: IFRAME_ID,
+      payload: {
+        playerId: "36416", // PlayerId
+        duration: 10000, // duration in milliseconds
+        resolution: {
+          width: 1080,
+          height: 1920,
+        },
+        location: {
+          name: "My first location",
+          lat: 52.340264,
+          lng: 4.842859,
+        },
+      },
+    };
+    this.sendMessage(configuration);
+    console.log(configuration);
+  };
+
+  sendMessage = (msg) => {
+    const iframeWin = document.getElementById(IFRAME_ID).contentWindow;
+    if (iframeWin) {
+      iframeWin.postMessage(msg, "*");
+    }
+  };
+  render() {
+    return (
+      <iframe
+        id={IFRAME_ID}
+        title="Webcontainer"
+        scrolling="no"
+        src={url}
+        style={{
+          height: "100%",
+          width: "100%",
+          border: "none",
+          position: "absolute",
+        }}
+        onLoad={this.onLoad}
+      />
+    );
+  }
 }
-
 export default App;
